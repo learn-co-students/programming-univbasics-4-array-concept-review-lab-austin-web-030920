dd5d4b0e39d1127fa1bbeb073511f9322ffc7799
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-11 02:03:46 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,16 +1,30 @@
 def find_element_index(array, value_to_find)
-  array.length.times do |count|
-  if array[count] == value_to_find 
-    return count 
+  array.length.times do |donna|
+  if array[donna] == value_to_find 
+    return donna 
   end
 end
 nil
 end
 
-# def find_max_value(array)
+def find_max_value(array)
+  x = array[0]
+  array.length.times do |party|
+    if array[party] > x
+   x = array[party]
+  end     
 #   array.max 
-# end
+end
+x
+end
 
-# def find_min_value(array)
-#   array.min
-# end
+def find_min_value(array)
+  x = array[0]
+  array.length.times do |love|
+    if array[love] < x 
+      x = array[love]
+    end
+  end
+  x
+#  array.min
+end
